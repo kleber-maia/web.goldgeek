56472f519c2441c70966940b10807471526dccf4
Kleber Maia <kleber@soft.group>
2026-04-11 13:05:51 -0400

    Fix appraisal form build error

--- src/app/(main)/request-appraisal/page.tsx
+++ src/app/(main)/request-appraisal/page.tsx
@@ -72,7 +72,9 @@ export default function RequestAppraisalPage() {
   };
 
   const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
   ) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
